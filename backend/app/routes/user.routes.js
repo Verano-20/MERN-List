da387aca887b7dd5b2e9bebd15ee54e384498a8d
Verano-20 <samdawson97@hotmail.co.uk>
2021-01-13 16:42:42 +0000

    MERNLI-30 added user.routes.js

---
+++ backend/app/routes/user.routes.js
@@ -0,0 +1,20 @@
+const {authJwt} = require('../middleware');
+const controller = require('../controllers/user.controller');
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+            'Access-Control-Allow-Headers',
+            'x-access-token, Origin, Content-Type, Accept'
+        );
+        next();
+    });
+
+    app.get('/user/getList', controller.getList);
+
+    app.post('/user/newTask', controller.newTask);
+
+    app.post('/user/deleteTask', controller.deleteTask);
+
+    app.post('/user/completedTask', controller.completedTask);
+};
