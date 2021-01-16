0f2c0a2fdfc8780d43fb2a6f5a9f829eb0a6768e
Verano-20 <samdawson97@hotmail.co.uk>
2021-01-16 10:30:58 +0000

    getList route completed

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -3,8 +3,41 @@ const User = db.user;
 const List = db.list;
 
 exports.getList = (req, res) => {
-    res.status(200).send('getList');
-}
+    // request validation
+    if (!(req.body.username && req.body.listId)) {
+        res.status(400).send({message: "Invalid requeset."});
+        return;
+    }
+
+    // find user
+    User.findOne({
+        username: req.body.username
+    })
+    .populate('roles', '-__v')
+    .exec((err, user) => {
+        if (err) {
+            res.status(500).send({message: err});
+            return;
+        }
+
+        if (!user) {
+            return res.status(404).send({message: "User not found."});
+        }
+
+        if (!user.lists.includes(req.body.listId)) {
+            return res.status(403).send({message: "List does not belong to user."});
+        }
+
+        List.findById(req.body.listId).exec((err, list) => {
+            if (err) {
+                res.status(500).send({message: err});
+                return;
+            }
+
+            res.status(200).send(list.items);
+        });
+    });
+};
 
 exports.newTask = (req, res) => {
     // request validation
