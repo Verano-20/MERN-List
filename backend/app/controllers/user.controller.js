c93139f6fb03ee6c2360b13ec01fe7e3896ca5e3
Verano-20 <samdawson97@hotmail.co.uk>
2021-01-17 12:02:18 +0000

    deleteTask route completed

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -74,7 +74,7 @@ exports.newTask = (req, res) => {
             }
 
             list.items.push({
-                itemID: req.body.itemId,
+                itemId: req.body.itemId,
                 title: req.body.itemTitle,
                 completed: false,
                 timeStamp: Date.now()
@@ -93,8 +93,62 @@ exports.newTask = (req, res) => {
 };
 
 exports.deleteTask = (req, res) => {
-    res.status(200).send('deleteTask');
-}
+    // request validation
+    if (!(req.body.username && req.body.listId && req.body.itemId)) {
+        res.status(400).send({message: "Invalid request."});
+        return;
+    }
+
+    // find user
+    User.findOne({
+        username: req.body.username
+    })
+    .populate('roles', '-__v')
+    .exec((err, user) => { 
+        if (err) {
+            res.status(500).send({message: err});
+            return;
+        }
+        
+        if (!user) {
+            return res.status(404).send({message: "User not found."});
+        }
+
+        // check list is in user's lists
+        if (!user.lists.includes(req.body.listId)) {
+            return res.status(403).send({message: "List does not belong to user."})
+        }
+
+        // find list
+        List.findById(req.body.listId).exec((err, list) => {
+            if (err) {
+                res.status(500).send({message: err});
+                return;
+            }
+
+            for (let i = 0; i < list.items.length; i++) {
+                if (list.items[i]._id == req.body.itemId) {
+                    list.items.splice(i, 1);
+                    break;
+                }
+                
+                if (i == list.items.length - 1) {
+                    res.status(404).send({message: "List item not found."});
+                    return;
+                }
+            }
+
+            list.save(err => {
+                if (err) {
+                    res.status(500).send({message: err});
+                    return;
+                }
+
+                res.status(200).send({message: "List item deleted successfully."});
+            });
+        });
+    });
+};
 
 exports.completedTask = (req, res) => {
     res.status(200).send('completedTask');
